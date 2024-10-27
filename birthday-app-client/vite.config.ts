884148a7db98c710095665f500fc9a886a4f0bc4
Grant Sherman <tnarg122@gmail.com>
2024-10-27 05:24:43 +0000

    vite scaffold

---
+++ birthday-app-client/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import deno from '@deno/vite-plugin'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [deno(), react()],
+  preview: {
+    port: 3000
+  },
+  server: {
+    port: 3000
+  }
+})
