30c986acaf4990f80c3a90508b581138c5b9ac3e
Grant Sherman <tnarg122@gmail.com>
2024-11-17 22:24:52 +0000

    typescript eslint and trying to get to run in vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
-import deno from "@deno/vite-plugin";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
-        // deno(),
-        react({
-            include: "**/*.tsx",
-        }),
+        react(),
     ],
     preview: {
         port: 3000,
