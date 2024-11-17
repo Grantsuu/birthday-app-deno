71f5d4e4bee24fb7307a415133caa6bd7f545722
Grant Sherman <tnarg122@gmail.com>
2024-11-17 17:17:38 +0000

    package json, yarn lock, vite config for node

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
-        deno(),
+        // deno(),
         react({
             include: "**/*.tsx",
         }),
@@ -16,7 +16,7 @@ export default defineConfig({
     server: {
         port: 3000,
         watch: {
-            // usePolling: true,
+            usePolling: true,
         },
     },
 });
