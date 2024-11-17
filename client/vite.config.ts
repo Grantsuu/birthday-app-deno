fe99688c1c125ff928482ac3a29357dd8512e2c3
Grant Sherman <tnarg122@gmail.com>
2024-11-17 22:39:01 +0000

    full switch over

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
     },
     server: {
         port: 3000,
-        watch: {
-            usePolling: true,
-        },
+        // watch: {
+        //     usePolling: true,
+        // },
     },
 });
